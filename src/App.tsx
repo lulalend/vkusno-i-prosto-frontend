ca88da13f1ceef9603d6e1907e17fcf5a4f97a5e
Yana <yanakl@list.ru>
2024-10-28 21:32:06 +0300

    Move the modal into its own dir

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { Modal } from './components/Modal.tsx';
+import { Modal } from './components/modal/Modal.tsx';
 
 export const App = () => {
     const [authActive, setAuthActive] = useState(false);
