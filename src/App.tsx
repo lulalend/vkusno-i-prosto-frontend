11e5fe73faa016e7e536f0643b464b9f49cb7d7a
Yana <yanakl@list.ru>
2024-10-30 21:47:13 +0300

    Add search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import { useState } from 'react';
-import './App.css';
-import { Modal } from './components/modal/Modal.tsx';
-import { AuthPage } from './pages/auth/AuthPage.tsx';
+import { MainPage } from './pages/main/MainPage.tsx';
 
 export const App = () => {
-  const [authActive, setAuthActive] = useState(true);
-
   return (
-    <>
-      <button onClick={() => setAuthActive(true)} className="button">
-        Log In
-      </button>
-      {authActive && (
-        <Modal isActive={authActive} setIsActive={setAuthActive}>
-          <AuthPage />
-        </Modal>
-      )}
-    </>
+    <MainPage />
   );
 };
---
+++ src/pages/main/MainPage.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+export const MainPage = () => (
+  <div className={styles.container}>
+    <div className={styles.search}>
+      <input/>
+    </div>
+  </div>
+);
