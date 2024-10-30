c18de5388b7d7a3b6eefc0ba55eeffe85fd64fca
Yana <yanakl@list.ru>
2024-10-30 12:48:49 +0300

    Change design auth page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { Modal } from './components/modal/Modal.tsx';
 import { AuthPage } from './pages/auth/AuthPage.tsx';
@@ -10,26 +8,9 @@ export const App = () => {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setAuthActive(true)} className="button">
-          Log In
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => setAuthActive(true)} className="button">
+        Log In
+      </button>
       {authActive && (
         <Modal isActive={authActive} setIsActive={setAuthActive}>
           <AuthPage />
