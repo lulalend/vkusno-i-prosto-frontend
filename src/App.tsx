4a6f41b8593edfbff4087ef5749c4d7f2d23a03c
Yana <yanakl@list.ru>
2024-11-10 14:54:46 +0300

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MainPage } from './pages/main/MainPage.tsx';
+import { Header } from './components/header/Header.tsx';
 
 export const App = () => (
   <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<MainPage />} />
     </Routes>
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './styles.module.css';
+import { Modal } from '../modal/Modal.tsx';
+import { AuthPage } from '../../pages/auth/AuthPage.tsx';
+
+export const Header = () => {
+  const navigate = useNavigate();
+  const [isAuthActive, setIsAuthActive] = useState<boolean>(false);
+
+  return (
+    <div className={styles.headerContainer}>
+      <span className={styles.headerTitle} onClick={() => navigate('/')}>
+        Вкусно и Просто
+      </span>
+      <div className={styles.headerAuth} onClick={() => setIsAuthActive(true)}>
+        Войти
+      </div>
+
+      {isAuthActive && (
+        <Modal isActive={isAuthActive} setIsActive={setIsAuthActive}>
+          <AuthPage />
+        </Modal>
+      )}
+    </div>
+  );
+};
