bb28045f02f49008d39cc29c8c598eb4caf1bac3
Yana <yanakl@list.ru>
2024-11-10 15:01:36 +0300

    Add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MainPage } from './pages/main/MainPage.tsx';
 import { Header } from './components/header/Header.tsx';
+import { NoMatch } from './pages/404/NoMatch.tsx';
 
 export const App = () => (
   <BrowserRouter>
     <Header />
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="*" element={<NoMatch />}/>
     </Routes>
   </BrowserRouter>
 );
