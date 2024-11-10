8cea62c8d894ab33f85d3ae5960a21da48b1197a
Yana <yanakl@list.ru>
2024-11-10 14:51:41 +0300

    Add modal

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+import styles from './styles.module.css';
+
+type Props = {
+  isActive: boolean;
+  setIsActive: (value: boolean) => void;
+  children: ReactNode;
+};
+
+export const Modal = ({ isActive, setIsActive, children }: Props) => (
+  <div
+    className={`${styles.container} ${isActive ? styles.active : ''}`}
+    onClick={() => setIsActive(false)}
+  >
+    <div
+      className={`${styles.modalContent} ${isActive ? styles.active : ''}`}
+      onClick={(e) => e.stopPropagation()}
+    >
+      {children}
+    </div>
+  </div>
+);
