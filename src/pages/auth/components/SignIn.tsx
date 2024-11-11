7737f958363039db97f445180619334d5af055c4
Yana <yanakl@list.ru>
2024-11-11 17:43:19 +0300

    Add user api realisation

--- src/pages/auth/components/SignIn.tsx
+++ src/pages/auth/components/SignIn.tsx
@@ -1,11 +1,38 @@
+import { useState } from 'react';
 import styles from '../styles.module.css';
+import { useSignIn } from '../../../api/user/useSignIn.ts';
 
-export const SignIn = () => (
-  <>
-    <div className={styles.inputs}>
-      <input type="text" placeholder="Логин" name="login" required />
-      <input type="password" placeholder="Пароль" name="password" required />
-    </div>
-    <button className={styles.submitBtn}>Войти</button>
-  </>
-);
+export const SignIn = () => {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const { mutate: signIn } = useSignIn();
+
+  const handleSignIn = () => {
+    signIn({login, password});
+
+    setLogin('');
+    setPassword('');
+  };
+
+  return (
+    <>
+      <div className={styles.inputs}>
+        <input
+          type="text"
+          placeholder="Логин"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Пароль"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button className={styles.submitBtn} onClick={handleSignIn}>Войти</button>
+    </>
+  );
+};
--- src/pages/auth/components/SignUp.tsx
+++ src/pages/auth/components/SignUp.tsx
@@ -1,17 +1,57 @@
+import { useState } from 'react';
 import styles from '../styles.module.css';
+import { useSignUp } from '../../../api/user/useSignUp.ts';
+import { useSignIn } from '../../../api/user/useSignIn.ts';
 
-export const SignUp = () => (
-  <>
-    <div className={styles.inputs}>
-      <input type="text" placeholder="Логин" name="login" required />
-      <input
-        type="text"
-        placeholder="Имя пользователя"
-        name="username"
-        required
-      />
-      <input type="password" placeholder="Пароль" name="password" required />
-    </div>
-    <button className={styles.submitBtn}>Стать частью</button>
-  </>
-);
+export const SignUp = () => {
+  const [login, setLogin] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const { mutate: signUp } = useSignUp();
+  const { mutate: signIn } = useSignIn();
+
+  const handleSignUp = () => {
+    const data = {
+      login,
+      username,
+      password,
+    };
+
+    signUp(data, {
+      onSuccess: () => {
+        signIn({ login, password });
+      },
+    });
+  };
+
+  return (
+    <>
+      <div className={styles.inputs}>
+        <input
+          type="text"
+          placeholder="Логин"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Имя пользователя"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Пароль"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button className={styles.submitBtn} onClick={handleSignUp}>
+        Стать частью
+      </button>
+    </>
+  );
+};
