02638f97c5bd79206bc3b64c0caaae38e32d6a1d
Yana <yanakl@list.ru>
2024-11-12 13:55:35 +0300

    Add profile icon

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,19 +3,29 @@ import { useNavigate } from 'react-router-dom';
 import styles from './styles.module.css';
 import { Modal } from '../modal/Modal.tsx';
 import { AuthPage } from '../../pages/auth/AuthPage.tsx';
+import Profile from '../../assets/svg/profile.svg';
 
 export const Header = () => {
   const navigate = useNavigate();
   const [isAuthActive, setIsAuthActive] = useState<boolean>(false);
+
+  const token = localStorage.getItem('token');
 
   return (
     <div className={styles.headerContainer}>
       <span className={styles.headerTitle} onClick={() => navigate('/')}>
         Вкусно и Просто
       </span>
-      <div className={styles.headerAuth} onClick={() => setIsAuthActive(true)}>
-        Войти
-      </div>
+      {token ? (
+        <img className={styles.profileIcon} src={Profile} alt="Profile icon" />
+      ) : (
+        <div
+          className={styles.headerAuth}
+          onClick={() => setIsAuthActive(true)}
+        >
+          Войти
+        </div>
+      )}
 
       {isAuthActive && (
         <Modal isActive={isAuthActive} onClose={() => setIsAuthActive(false)}>
