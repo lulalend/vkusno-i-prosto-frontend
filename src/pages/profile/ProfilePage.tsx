d99f5beb558f95a07ebb95fb88ad2b5244cdaa70
Yana <yanakl@list.ru>
2024-11-13 12:26:45 +0300

    Add profile style

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,5 +1,9 @@
-export const ProfilePage = () => (
-  <div className="container">
+import styles from './styles.module.css';
 
+export const ProfilePage = () => (
+  <div className={styles.profilePage}>
+    <div className={styles.container}>
+
+    </div>
   </div>
 );
