bde5b22bee6fa71dbb8dc6a2a971f3b1aa8658f8
Yana <yanakl@list.ru>
2024-11-15 11:07:59 +0300

    Add recipe container

---
+++ src/components/recipes/container/RecipeContainer.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.css';
+import { Recipe } from '../../../types/types.ts';
+import { RecipeCard } from '../card/RecipeCard.tsx';
+
+type Props = {
+  recipes: Recipe[];
+};
+
+export const RecipeContainer = ({ recipes }: Props) => (
+  <div className={styles.recipeContainer}>
+    {recipes &&
+      recipes.length > 0 &&
+      recipes.map((recipe) => <RecipeCard key={recipe.id} recipe={recipe} />)}
+  </div>
+);
