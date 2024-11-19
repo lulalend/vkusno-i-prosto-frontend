de1e504c8f3c73ce969ed9956102a287054fd3b9
Yana <yanakl@list.ru>
2024-11-19 21:09:42 +0300

    Add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { App } from './App.tsx';
 import './index.css';
+import './sentry';
 import { queryClient } from './queryClient.ts';
 
 createRoot(document.getElementById('root')!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: 'itmo-ki',
+      project: 'vkusno-i-prosto',
+    }),
+  ],
+
   assetsInclude: ['**/*.lottie'],
+
+  build: {
+    sourcemap: true,
+  },
 });
