240529b17f6fa56044b4cb19079604ca3a693a70
Yana <yanakl@list.ru>
2024-11-19 12:20:43 +0300

    Add layout

--- src/pages/404/NoMatch.tsx
+++ src/pages/404/NoMatch.tsx
@@ -1,3 +1,14 @@
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import styles from './styles.module.css';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import NotFound from '../../assets/lotties/NotFound.lottie';
+
 export const NoMatch = () => (
-  <div>Упс... Страница не найдена, может перейдём на главную?</div>
+  <div className={styles.container}>
+    <DotLottieReact src={NotFound} loop autoplay className={styles.lottie} />
+    <div className={styles.text}>
+      Упс... Страница не найдена, может перейдём на главную?
+    </div>
+  </div>
 );
